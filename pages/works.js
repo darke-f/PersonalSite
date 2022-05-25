30ce06f3c0a627cea3e322bf31cfa0bcbff93ec9
darke-f <ariiq.pooka@gmail.com>
2022-05-25 20:54:02 +0700

    removed the poop

--- pages/works.js
+++ pages/works.js
@@ -2,9 +2,8 @@ import { Container, Heading } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPoop } from '@fortawesome/free-solid-svg-icons'
-import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
+import { DeleteIcon } from '@chakra-ui/icons'
+
 
 
 const Works = () => {
@@ -16,7 +15,7 @@ const Works = () => {
                 </Heading>
                 <Section delay={0.1}>
                     <Paragraph>
-                        This page is a dud <FontAwesomeIcon icon={faPoop} />. I haven&apos;t done any full time jobs as of now. I&apos;ve only done internship and freelance work before, which i don&apos;t consider as a full time job.
+                        This page is a dud <DeleteIcon />. I haven&apos;t done any full time jobs as of now. I&apos;ve only done internship and freelance work before, which i don&apos;t consider as a full time job.
                         When i get a job i&apos;ll update this page accordingly, until then stay tuned!
                     </Paragraph>
                 </Section>
